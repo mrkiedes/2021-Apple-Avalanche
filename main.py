3149282059bef32c5742aa0fe9c0e65151235037
Mr. Kiedes <akiedes@vtsd.com>
2021-11-03 11:21:17 -0400

    Apple Update

--- main.py
+++ main.py
@@ -3,24 +3,38 @@ import turtle as trtl
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
-
+xOffset = -20
+yOffset = -47
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 wn.bgpic("background.gif")
 apple = trtl.Turtle()
-
-
+wn.tracer(False)
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
+  active_apple.penup()
   active_apple.shape(apple_image)
+  draw_letter("A", active_apple)
   wn.update()
 
 def appledrop():
+  wn.tracer(True)
   apple.penup()
-  apple.goto(active_apple.xcor(), -250)
+  apple.goto(apple.xcor(), -250)
+  wn.tracer(False)
+
+def draw_letter(letter, active_apple):
+  active_apple.color("white")
+  rememberPos = active_apple.position()
+  active_apple.setpos(active_apple.xcor() + xOffset, active_apple.ycor() + yOffset)
+  active_apple.write(letter, font=("Arial", 50, "bold"))
+  active_apple.setpos(rememberPos)
+
+
+
 
 #-----function calls-----
 draw_apple(apple)
